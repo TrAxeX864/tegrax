61e834979495da4b01f94f541db88048da0fe655
traxex864 <traxex864@gmail.com>
2021-08-23 17:18:50 +0700

    Update quote worker

--- TgVozderzhansBot/Core/Workers/QuoteWorker.cs
+++ TgVozderzhansBot/Core/Workers/QuoteWorker.cs
@@ -34,38 +34,45 @@ namespace TgVozderzhansBot.Core.Workers
                 
                     foreach (var user in users)
                     {
-                        int attempts = 0;
-                    
-                        m1:
-
-                        var skip = (int)(rand.NextDouble() * db.Quotes.Count());
-                        var quote = db.Quotes.OrderBy(x => x.Id).Skip(skip).Take(1).FirstOrDefault();
-
-                        int cc = db.QuoteTables.Count(x => x.User == user && x.UpdatedAt > DateTime.Now.AddDays(-1));
-                    
-                        if (quote != null && cc == 0)
+                        try
                         {
-                            int c = db.QuoteTables.Count(x => x.User == user && x.LastQuote == quote);
+                            int attempts = 0;
+                    
+                            m1:
 
-                            if (c == 0)
+                            var skip = (int)(rand.NextDouble() * db.Quotes.Count());
+                            var quote = db.Quotes.OrderBy(x => x.Id).Skip(skip).Take(1).FirstOrDefault();
+
+                            int cc = db.QuoteTables.Count(x => x.User == user && x.UpdatedAt > DateTime.Now.AddDays(-1));
+                    
+                            if (quote != null && cc == 0)
                             {
-                                var quoteTable = new QuoteTable();
-                                quoteTable.User = db.Users.FirstOrDefault(x => x.Id == user.Id);
-                                quoteTable.LastQuote = quote;
-                                quoteTable.UpdatedAt = DateTime.Now;
-                            
-                                await db.QuoteTables.AddAsync(quoteTable);
-                                await db.SaveChangesAsync();
+                                int c = db.QuoteTables.Count(x => x.User == user && x.LastQuote == quote);
 
-                                await _botClient.SendTextMessageAsync(user.ChatId, "<b>Цитата дня</b>\n\n" + quote.Text + $"\n\n<i>{quote.Name}</i>", ParseMode.Html);
+                                if (c == 0)
+                                {
+                                    var quoteTable = new QuoteTable();
+                                    quoteTable.User = db.Users.FirstOrDefault(x => x.Id == user.Id);
+                                    quoteTable.LastQuote = quote;
+                                    quoteTable.UpdatedAt = DateTime.Now;
+                            
+                                    await db.QuoteTables.AddAsync(quoteTable);
+                                    await db.SaveChangesAsync();
+
+                                    await _botClient.SendTextMessageAsync(user.ChatId, "<b>Цитата дня</b>\n\n" + quote.Text + $"\n\n<i>{quote.Name}</i>", ParseMode.Html);
+                                }
+                                else
+                                {
+                                    attempts++;
+                            
+                                    if(attempts < 5)
+                                        goto m1;
+                                }                        
                             }
-                            else
-                            {
-                                attempts++;
-                            
-                                if(attempts < 5)
-                                    goto m1;
-                            }                        
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("Quote worker user error: " + ex.Message);
                         }
                     }   
                     
